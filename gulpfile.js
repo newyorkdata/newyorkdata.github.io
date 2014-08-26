aaf81263e74a984fd376af3c44791e11a8253aec
Matthew Conlen <mc@mathisonian.com>
2014-08-26 18:53:17 -0400

    add local gulpfile for testing

---
+++ gulpfile.js
@@ -0,0 +1,133 @@
+'use strict';
+
+var gulp = require('gulp');
+var browserify = require('gulp-browserify');
+var sass = require('gulp-sass');
+var csso = require('gulp-csso');
+var livereload = require('gulp-livereload');
+var tinylr = require('tiny-lr');
+var server = tinylr();
+var jade = require('gulp-jade');
+var path = require('path');
+var express = require('express');
+var app = express();
+var gutil = require('gulp-util');
+var gulpif = require('gulp-if');
+var uglify = require('gulp-uglify');
+var s3 = require('gulp-s3');
+var gzip = require('gulp-gzip');
+var colors = require('colors');
+var rename = require('gulp-rename');
+var _ = require('lodash');
+
+
+var PRODUCTION_MODE = gutil.env.production;
+var projectName = require('./package').name;
+
+
+gulp.task('js-lib', function() {
+    return gulp.src('./js/lib/**/*.js')
+            .pipe(gulpif(PRODUCTION_MODE, uglify()))
+            .pipe(gulp.dest('./public/js/lib/'))
+            .pipe( livereload( server ));
+});
+
+gulp.task('gzip', ['build'], function() {
+    return gulp.src('public/**/*.{js,css}')
+                .pipe(gzip())
+                .pipe(rename(function(path) {
+                    path.extname = '';
+                }))
+                .pipe(gulp.dest('./public/'));
+});
+
+gulp.task('browserify', function() {
+    // Single entry point to browserify
+    return gulp.src('./js/app.js')
+        .pipe(browserify({
+            debug : !PRODUCTION_MODE
+        }))
+        .on('error', gutil.log)
+        .on('error', gutil.beep)
+        .pipe(gulpif(PRODUCTION_MODE, uglify()))
+        .pipe(gulp.dest('./public/js/'))
+        .pipe( livereload( server ));
+});
+
+
+gulp.task('css', function() {
+    return gulp
+            .src('./stylesheets/app.scss')
+            .pipe(
+                sass({
+                    includePaths: ['./stylesheets'],
+                    errLogToConsole: true
+                }))
+            .pipe( gulpif(PRODUCTION_MODE, csso()) )
+            .pipe( gulp.dest('./public/css/') )
+            .pipe( livereload( server ));
+});
+
+
+gulp.task('fonts', function() {
+    return gulp
+            .src('./fonts/**/*.{otf,svg,ttf,woff,eot}')
+            .pipe( gulp.dest('./public/fonts/') )
+            .pipe( livereload( server ));
+});
+
+gulp.task('images', function() {
+    return gulp
+            .src('./images/**/*')
+            .pipe( gulp.dest('./public/images/') )
+            .pipe( livereload( server ));
+});
+
+gulp.task('jade', function() {
+
+    // delete require.cache[require.resolve('./js/utils')];
+    // var utils = require('./js/utils');
+    
+    return gulp.src('./templates/index.jade')
+               .pipe(jade({
+                    pretty: !PRODUCTION_MODE,
+                    locals: {
+                        _: _
+                    }
+               }))
+               .on('error', gutil.log)
+               .on('error', gutil.beep)
+               .pipe(gulp.dest('./public/'))
+               .pipe( livereload( server ));
+});
+
+gulp.task('templates', ['static'], function() {
+    return gulp.start('jade');
+});
+
+
+gulp.task('express', function() {
+    app.use(express.static(path.resolve('./public')));
+    app.listen(8000);
+    gutil.log('Listening on port: 8000');
+});
+ 
+gulp.task('watch', function () {
+  server.listen(35729, function (err) {
+    if (err) {
+      return console.log(err);
+    }
+ 
+    gulp.watch('./stylesheets/**/*.{scss,css}',['css', 'jade']);
+    gulp.watch('./js/**/*.js',['js']);
+    gulp.watch('./templates/**/*.jade',['jade', 'js']);
+  });
+});
+
+
+
+
+gulp.task('js', ['browserify','js-lib']);
+gulp.task('static', ['js', 'css', 'fonts', 'images']);
+gulp.task('default', ['templates','express','watch']);
+gulp.task('build', ['templates']);
